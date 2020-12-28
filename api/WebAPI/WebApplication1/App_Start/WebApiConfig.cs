fdb96040865ec329219a9f37ad1b31b62ee59214
abdullah abdullah <abdullah2004@live.ca>
2020-12-28 11:12:35 -0500

    1st commit

---
+++ api/WebAPI/WebApplication1/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Net.Http.Headers;
+using System.Web.Http.Cors;
+
+namespace WebApplication1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            //if you want to whitelist a website you can add google.com so only google.com can access your api's
+        }
+    }
+}
